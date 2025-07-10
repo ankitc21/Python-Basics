362449b8af7510dddb2c4ca073454db2dffbd70c
ankitc21 <ankit004c@gmail.com>
2025-07-11 00:05:33 +0530

    Add files via upload

---
+++ Numpy/Array_sort.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([3, 2, 0, 1])
+
+print(np.sort(arr))
+
+arr = np.array(['banana', 'cherry', 'apple'])
+print(np.sort(arr))
+
+arr=np.array([[36,4,10],[9,6,8]])
+print(np.sort(arr))
